88d18ca1953ffb0da1bb74a5d9e3722cdedebc13
Greg Palen <codingvirtual@gmail.com>
2018-03-19 16:23:10 -0500

    updated leaf size to .01 added PassThru filter code

--- Exercise-1/RANSAC.py
+++ Exercise-1/RANSAC.py
@@ -11,7 +11,7 @@ vox = cloud.make_voxel_grid_filter()
 # Choose a voxel (also known as leaf) size
 # Note: this (1) is a poor choice of leaf size
 # Experiment and find the appropriate size!
-LEAF_SIZE = 5
+LEAF_SIZE = .01
 
 # Set the voxel (or leaf) size
 vox.set_leaf_size(LEAF_SIZE, LEAF_SIZE, LEAF_SIZE)
@@ -22,7 +22,21 @@ filename = 'voxel_downsampled.pcd'
 pcl.save(cloud_filtered, filename)
 
 # PassThrough filter
+# PassThrough filter
+# Create a PassThrough filter object.
+passthrough = cloud_filtered.make_passthrough_filter()
 
+# Assign axis and range to the passthrough filter object.
+filter_axis = 'z'
+passthrough.set_filter_field_name(filter_axis)
+axis_min = 0
+axis_max = 2
+passthrough.set_filter_limits(axis_min, axis_max)
+
+# Finally use the filter function to obtain the resultant point cloud.
+cloud_filtered = passthrough.filter()
+filename = 'pass_through_filtered.pcd'
+pcl.save(cloud_filtered, filename)
 
 # RANSAC plane segmentation
 
