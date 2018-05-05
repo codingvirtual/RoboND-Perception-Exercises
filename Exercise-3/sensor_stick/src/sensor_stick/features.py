f168624bb815b9fbbe5727fa2901f0d1595a306e
Greg Palen <codingvirtual@gmail.com>
2018-05-05 17:00:30 -0500

    change nbins default to 128

--- Exercise-3/sensor_stick/src/sensor_stick/features.py
+++ Exercise-3/sensor_stick/src/sensor_stick/features.py
@@ -10,7 +10,7 @@ def rgb_to_hsv(rgb_list):
     return hsv_normalized
 
 
-def compute_color_histograms(cloud, using_hsv=False, nbins=32, range=(0, 256)):
+def compute_color_histograms(cloud, using_hsv=False, nbins=128, range=(0, 256)):
 
     # Compute histograms for the clusters
     point_colors_list = []
@@ -47,7 +47,7 @@ def compute_color_histograms(cloud, using_hsv=False, nbins=32, range=(0, 256)):
     return normed_features 
 
 
-def compute_normal_histograms(normal_cloud, nbins = 32, range = range):
+def compute_normal_histograms(normal_cloud, nbins=128, range=range):
     norm_x_vals = []
     norm_y_vals = []
     norm_z_vals = []
